5c2ed13600fd9d343d4f0de6cb4cfd21c5282bdd
dima cheban <dimacheban23@gmail.com>
2025-03-01 16:49:45 +0200

    thirth commit

--- test/test_main.py
+++ test/test_main.py
@@ -9,10 +9,21 @@ def client():
 
 def test_index(client):
     response = client.get("/")
-    
-
     assert response.status_code == 200
-    
     json_response = response.json()
     assert json_response["message"] == "index page"
     assert "base_url" in json_response
+
+def test_index_2(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    json_response = response.json()
+    assert json_response["message"] != "indexx page"
+    assert "base_url_fake" not in json_response
+
+def test_index_3(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    json_response = response.json()
+    assert json_response["message"] != "index page fake"
+    assert "base_url_fake" not in json_response
