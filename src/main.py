e6037cdb8bdd3ec42dd6b3c3d84b92abfadef474
dima cheban <dimacheban23@gmail.com>
2025-03-01 16:33:43 +0200

    fist commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+@app.get('/')
+async def index(request:Request):
+    return {
+        'message':'index page',
+        'base_url': request.base_url
+    }
