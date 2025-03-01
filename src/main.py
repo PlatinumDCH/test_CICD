0800bfff0a3d50e42025e72b4efe37eb15e18bb5
dima cheban <dimacheban23@gmail.com>
2025-03-01 20:00:37 +0200

    update commit

--- src/main.py
+++ src/main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
+
 @app.get('/')
-async def index(request:Request):
+async def index(request: Request):
     """docs"""
     return {
-        'message' : 'index page', 
-        'base_url' : request.base_url
+        'message': 'index page',
+        'base_url': request.base_url
     }
--- test/test_main.py
+++ test/test_main.py
@@ -3,10 +3,12 @@ from fastapi.testclient import TestClient
 
 from src.main import app
 
+
 @pytest.fixture
 def client():
     client = TestClient(app)
     return client
+
 
 def test_index(client):
     response = client.get("/")
@@ -15,6 +17,7 @@ def test_index(client):
     assert json_response["message"] == "index page"
     assert "base_url" in json_response
 
+
 def test_index_2(client):
     response = client.get("/")
     assert response.status_code == 200
@@ -22,10 +25,10 @@ def test_index_2(client):
     assert json_response["message"] != "indexx page"
     assert "base_url_fake" not in json_response
 
+
 def test_index_3(client):
     response = client.get("/")
     assert response.status_code == 200
     json_response = response.json()
     assert json_response["message"] != "index page fake"
     assert "base_url_fake" not in json_response
-    
