d34bd7e5ea64d9f69a10f81d2e89e5819874a63a
dima cheban <dimacheban23@gmail.com>
2025-03-01 16:47:50 +0200

    second commit

---
+++ test/test_main.py
@@ -0,0 +1,18 @@
+import pytest
+from fastapi.testclient import TestClient
+from src.main import app
+
+@pytest.fixture
+def client():
+    client = TestClient(app)
+    return client
+
+def test_index(client):
+    response = client.get("/")
+    
+
+    assert response.status_code == 200
+    
+    json_response = response.json()
+    assert json_response["message"] == "index page"
+    assert "base_url" in json_response
